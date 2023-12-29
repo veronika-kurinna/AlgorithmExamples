f58d490cf2af75a34a533985faa691430071f0f0
Veronika Kurinna <kurinna.dev@gmail.com>
2023-12-29 19:42:20 +0200

    Change game GuessNumber

--- AlgorithmExamples/Games/GuessNumber.cs
+++ AlgorithmExamples/Games/GuessNumber.cs
@@ -4,15 +4,22 @@ namespace AlgorithmExamples.Games
 {
     public class GuessNumber : IGame
     {
-        private readonly int[] _numbers = { 1, 3, 5, 8, 10, 11, 14, 17, 20, 23, 28, 32, 37, 40, 49};
-
         public void Play()
         {
+            Random random = new Random();
+            int[] numbers = new int[15];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int randomNumber = random.Next(0, 50);
+                numbers[i] = randomNumber;
+            }
+            ArrayAlgorithms.SortElementsBubble(numbers);
+           
             Console.WriteLine("Write number from 1 to 50. If this number is present in array, program will find position of number");
             string? selectedNumberString = Console.ReadLine();
             int selectedNumber = Convert.ToInt32(selectedNumberString);
 
-            int index = BinarySearch.Search(_numbers, selectedNumber);
+            int index = BinarySearch.Search(numbers, selectedNumber);
             if (index == -1)
             {
                 Console.WriteLine("Selected number doesn't exist in array");
