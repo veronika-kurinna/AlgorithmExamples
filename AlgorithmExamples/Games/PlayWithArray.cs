45b325c75f164710a3f5e5557fe92699d5cb3aca
Veronika Kurinna <kurinna.dev@gmail.com>
2023-12-29 19:23:51 +0200

    Add method ReadNumber

--- AlgorithmExamples/Games/PlayWithArray.cs
+++ AlgorithmExamples/Games/PlayWithArray.cs
@@ -7,23 +7,32 @@ namespace AlgorithmExamples.Games
         public void Play()
         {
             Console.WriteLine("Write 5 numbers and computer will find the smallest number, the biggest number and sum of elements.");
-            string? firstNumberString = Console.ReadLine();
-            int firstNumber = Convert.ToInt32(firstNumberString);
-            string? secondNumberString = Console.ReadLine();
-            int secondNumber = Convert.ToInt32(secondNumberString);
-            string? thirdNumberString = Console.ReadLine();
-            int thirdNumber = Convert.ToInt32(thirdNumberString);
-            string? fourthNumberString = Console.ReadLine();
-            int fourthNumber = Convert.ToInt32(fourthNumberString);
-            string? fifthNumberString = Console.ReadLine();
-            int fifthNumber = Convert.ToInt32(fifthNumberString);
-
-            int[] array = { firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber };
+            int[] array = ReadNumber(5);
 
             ArraySpecification result = ArrayAlgorithms.CalculateSumElementsFindSmallestBiggestElement(array);
             Console.WriteLine($"The the smallest number: {result.TheSmallestNumber}");
             Console.WriteLine($"The the biggest number: {result.TheBiggestNumber}");
             Console.WriteLine($"Sum of elements: {result.Sum}");
+        }
+
+        public int[] ReadNumber(int countNumbers)
+        {
+            int[] array = new int[countNumbers];
+            for (int i = 0; i < countNumbers; i++)
+            {
+                string? numberString = Console.ReadLine();
+
+                if (numberString.All(char.IsNumber))
+                {
+                    int number = Convert.ToInt32(numberString);
+                    array[i] = number;
+                }
+                else
+                {
+                    throw new ArgumentException("You've written letters. Use only numbers");
+                }
+            }
+            return array;
         }
     }
 }
